c0eff3f6eb1f7cdb3241e38dce6d87b3a8fbc449
SeungEunChun <1000tmddms@gmail.com>
2024-01-29 15:42:44 +0900

    빌드 들어올렸어요!

--- web.js
+++ web.js
@@ -5,13 +5,13 @@ const path = require("path");
 
 app.use(express.static(path.join(__dirname, 'build')))
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, "bulld/index.html"))
+    res.sendFile(path.join(__dirname, "build/index.html"))
 })
 
 app.use((req, res) => {
     res.status(404).sendFile(path.join(__dirname, "publish/nopage.html"))
 })
 
-app.listen(6118, () => {
+app.listen(port, () => {
     console.log(`localhost ${port} onload`)
 })

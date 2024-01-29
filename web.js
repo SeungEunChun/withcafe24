0fd0d07f199a18e806ba23f6097f40a28ff3bcf0
SeungEunChun <1000tmddms@gmail.com>
2024-01-29 15:24:42 +0900

    빌드 옮겼어요!

---
+++ web.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 6118;
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, 'build')))
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "bulld/index.html"))
+})
+
+app.use((req, res) => {
+    res.status(404).sendFile(path.join(__dirname, "publish/nopage.html"))
+})

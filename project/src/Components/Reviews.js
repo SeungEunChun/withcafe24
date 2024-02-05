738e79477a2c194564dd38492d238bff971ef715
SeungEunChun <1000tmddms@gmail.com>
2024-02-05 12:42:36 +0900

    리뷰탭 전면수정/반응형 재구성 예정

--- project/src/Components/Reviews.js
+++ project/src/Components/Reviews.js
@@ -8,47 +8,26 @@ function Reviews(props) {
                 <p>싸이닉을 경험해 보신 고객님들의 리얼 후기들 입니다. <br />
                     베스트 후기에 선정되시면 포인트를 적립해드립니다.</p>
             </div>
+
             <div>
-                <div className='re_list row justify-content-between '>
+                <ul className='d-lg-flex row align-items-center'>
                     {
                         props.datasrc.map((e, i) => {
-                            return (
-                                <div key={i} className='col-lg-3 col-sm-6'>
-                                    <div className='re_box border' >
-
-                                        <a href="#none" className='d-block text-center'><img src={`./assets/review/${e.reviewimg}`} alt={e.alt} className='img-fluid' /></a>
-
-                                        <div className="re_info my-3 ps-3">
-                                            <p className='re_title'>
-                                                {e.ptitle}
-                                            </p>
-                                            <div className="re_txt">
-                                                {e.ptext}
-                                            </div>
-                                            <div className="re_star">
-                                                <span>
-                                                    {e.wr_id}
-                                                </span>
-                                                <span> </span>
-                                                <span>
-                                                    {e.star}
-                                                </span>
-                                            </div>
-                                        </div>
-                                        <div className="re_prd">
-                                            <a href="#none">
-                                                <img src="" alt="" />
-                                                <span>
-                                                </span>
-                                            </a>
-                                        </div>
+                            return <li className='d-block border mli'>
+                                <img src={`/assets/review/${e.reviewimg}`} alt={`review${i}`} className='img-fluid' />
+                                <div className='re_info'>
+                                    <strong className='d-block mb-3'>{e.ptitle}</strong>
+                                    <p>{e.ptext}</p>
+                                    <div className='d-flex'>
+                                        <p>{e.wr_id}</p>
+                                        <span className='ms-3'>{e.star}</span>
                                     </div>
+
                                 </div>
-                            )
+                            </li>
                         })
                     }
-
-                </div>
+                </ul>
             </div>
         </section>
     )
